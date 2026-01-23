d85f04d2bd34b284387d7d9c02e80839eef7b5b4
Abdallah Suallah <njedre@gmail.com>
2026-01-23 22:19:42 +0200

    hack

--- first_hackathon/hackathon_1.sql
+++
@@ -1,5 +0,0 @@
-alter table customer add column birth_date date;
-update customer set birth_date = current_date - 
-(floor(random() * (80 -18 +1)+18) * interval '1 year' 
- floor(random() * 365) * interval '1 day');
-select first_name, last_name, birth_date, email from customer;
