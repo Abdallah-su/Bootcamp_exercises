5550c7d84fcc9cd575fbcb1af11d60e499562146
Abdallah Suallah <njedre@gmail.com>
2026-02-01 10:32:18 +0200

    item

--- week5/day2/exercise_xp.sql
+++ week5/day2/exercise_xp.sql
@@ -1,14 +1,14 @@
-create database public
+CREATE DATABASE shop;
 CREATE TABLE items (item SERIAL PRIMARY KEY, name text not null, price numeric not null );
 CREATE TABLE customers (customer SERIAL PRIMARY KEY, first_name text not null, last_name text not null);
-insert in items (name, price)
-value('small desk', 100), ('large desk', 300), ('fan' 80);
-insert into customers(first_name, last_name)
-values('Greg', 'Jones'), ('Sandra', 'Jones'), ('Scott', 'Scott'), ('Trevor', 'Green'), ('Melanie', 'Johnson')
-select * from items;
-select * from items where price > 80;
-select * from items where price <= 300;
-select * from customers where last_name = 'Smith';
+INSERT INTO items (name, price)
+VALUES ('small desk', 100), ('large desk', 300), ('fan', 80);
+INSERT INTO customers(first_name, last_name)
+VALUES('Greg', 'Jones'), ('Sandra', 'Jones'), ('Scott', 'Scott'), ('Trevor', 'Green'), ('Melanie', 'Johnson');
+SELECT * FROM items;
+SELECT * FROM items where price > 80;
+SELECT * FROM items where price <= 300;
+SELECT * FROM customers where last_name = 'Smith';
 --no rows returned
-select * from customers where last_name ='Jones';
-select * from customers where last_name != 'Scott';
+SELECT * FROM customers where last_name ='Jones';
+SELECT * FROM customers where first_name != 'Scott';
