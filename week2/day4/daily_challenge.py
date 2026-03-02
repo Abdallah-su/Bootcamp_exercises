450988885a0116b48868cf8d925f934ad3b6f0ba
Abdallah Suallah <njedre@gmail.com>
2026-03-02 09:29:36 +0200

    weeek2day4daily

--- week2/day4/daily_challenge.py
+++ week2/day4/daily_challenge.py
@@ -1,4 +1,15 @@
-user_words = input( 'type in a list of words atleast 3 separated by ",": ' )
+#exercise 1
+user_words = input('type in a list of words atleast 3 separated by ",": ')
 words = user_words.split(',')
 sorted_word = sorted(words)
 print(','.join(sorted_word))
+
+#exercise 2
+def longest_word(sentence):
+    words = sentence.split(' ')
+    long_word = max(words, key =len)
+    print(long_word)
+
+sentence='type in a list of words atleast 3 separated by'
+longest_word(sentence)
+
