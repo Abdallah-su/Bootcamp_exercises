5450c563bab8afade2a322966b2e75ba29863bbc
Abdallah Suallah <njedre@gmail.com>
2026-05-12 09:52:58 +0300

    sql

---
+++ week17/day6/SQL_challenge.sql
@@ -0,0 +1,52 @@
+CREATE TABLE IF NOT EXISTS employees (
+    employee_id INT PRIMARY KEY,
+    employee_name VARCHAR(50),
+    salary DECIMAL(10, 2),
+    hire_date VARCHAR(20),
+    department VARCHAR(50)
+);
+
+INSERT INTO employees (employee_id, employee_name, salary, hire_date, department) VALUES
+(1, 'Amy West', 60000.00, '2021-01-15', 'HR'),
+(2, 'Ivy Lee', 75000.50, '2020-05-22', 'Sales'),
+(3, 'joe smith', 80000.75, '2019-08-10', 'Marketing'), 
+(4, 'John White', 90000.00, '2020-11-05', 'Finance'),
+(5, 'Jane Hill', 55000.25, '2022-02-28', 'IT'),
+(6, 'Dave West', 72000.00, '2020-03-12', 'Marketing'),
+(7, 'Fanny Lee', 85000.50, '2018-06-25', 'Sales'),
+(8, 'Amy Smith', 95000.25, '2019-11-30', 'Finance'),
+(9, 'Ivy Hill', 62000.75, '2021-07-18', 'IT'),
+(10, 'Joe White', 78000.00, '2022-04-05', 'Marketing'),
+(11, 'John Lee', 68000.50, '2018-12-10', 'HR'),
+(12, 'Jane West', 89000.25, '2017-09-15', 'Sales'),
+(13, 'Dave Smith', 60000.75, '2022-01-08', NULL),
+(14, 'Fanny White', 72000.00, '2019-04-22', 'IT'),
+(15, 'Amy Hill', 84000.50, '2020-08-17', 'Marketing'),
+(16, 'Ivy West', 92000.25, '2021-02-03', 'Finance'),
+(17, 'Joe Lee', 58000.75, '2018-05-28', 'IT'),
+(18, 'John Smith', 77000.00, '2019-10-10', 'HR'),
+(19, 'Jane Hill', 81000.50, '2022-03-15', 'Sales'),
+(20, 'Dave White', 70000.25, '2017-12-20', 'Marketing');
+
+SELECT * FROM employees;
+
+SELECT SUM(salary IS NULL ) as salary_nulls, 
+SUM(  department IS NULL ) AS depart_nulls,
+SUM(hire_date IS NULL) AS date_nulls from employees;
+
+UPDATE employees SET department='unknown' WHERE department is NULL;
+SELECT SUM(hire_date IS NULL) AS date_nulls from employees;
+
+--Check for and eliminate any duplicate rows in the dataset.
+SELECT *, COUNT(*) FROM employees GROUP by employee_id having COUNT(*) >1;
+
+--Correct any structural issues, such as inconsistent naming conventions or formatting errors.
+update employees set employee_name = UPPER(SUBSTR(1,1, employee_name))||SUBSTR(2, employee_name), hire_date = DATE(hire_date);
+
+--Detect and address any outliers that may skew the analysis.
+SELEct MAX(salary),MIN(salary),AVG(salary) FROM employees;
+SELECT *, NTILE(4) OVER (PARTITION BY department ORDER BY Salary) AS quantiles From employees;
+
+--Standardize and normalize data where applicable to ensure consistency
+SELECT DISTINCT department FROM employees
+
